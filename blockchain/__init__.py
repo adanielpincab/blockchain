06224da17c3b1b28cb4643bdd5be39067471be13
adanielpincab <adanielpincab@gmail.com>
2024-03-15 15:19:34 +0100

    Merkle tree

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -2,6 +2,8 @@ import crypto
 from time import time
 from hashlib import sha256
 from json import loads, dumps
+import sqlite3
+from math import ceil
 
 class Transaction:
     def __init__(self, addrFrom: str, addrTo: str, amount: int, fee=0):
@@ -22,6 +24,22 @@ class Transaction:
                 self.timestamp
             ]
         ).encode()).hexdigest()
+
+    def preHash(self):
+        '''
+        Binary transaction information for inserting the
+        transaction into a merkle tree.
+        Generates same hash in the tree than self.hash()
+        '''
+        return str(
+            [
+                self.addrFrom,
+                self.addrTo,
+                self.amount,
+                self.fee,
+                self.timestamp
+            ]
+        ).encode()
     
     def verify(self):
         return (
@@ -74,16 +92,93 @@ class Address:
             crypto.public_serialized(self.pub), 
             crypto.sign(str_data.encode(), self.priv)
         )
-    
+
 class Merkle:
-    def __init__(self):
-        self.l = []
+    def __init__(self, _list=None):
+        self.l = _list
+        self.levels = []
     
     def add(self, item):
-        self.l.append(item)
-        self.l.sort()
+        '''
+        Items ARE EXPECTED TO BE HASHED before adding the to the list
+        '''
+        if not self.l:
+            self.l = [item]
+        else:
+            self.l.append(item)
+        self.recalc()
+    
+    def recalc(self):
+        self.levels = [tuple(self.l)] # reset, first level
+        while len(self.levels[-1]) > 1:
+            print(self.levels[-1])
+            nextLevel = []
+            pair = []
+            for i in self.levels[-1]:
+                pair.append(i)
+                if len(pair) == 2:
+                    nextLevel.append(
+                        sha256((pair[0] + pair[1]).encode())
+                        .hexdigest()
+                    )
+                    pair = []
+            if len(pair) == 1:
+                nextLevel.append(pair[0])
+            self.levels.append(tuple(nextLevel))
+    
+    def root(self):
+        return self.levels[-1][0]
+    
+    def proof(self, hash):
+        res = [
+            ('hash', hash)
+        ]
+        for l in range(len(self.levels[:-1])):
+            level = self.levels[l]
+            ind = level.index(hash)
+            if (ind == len(level)-1) and (len(level)%2 != 0):
+                continue # last hash with no pairs just goes up to the next level
+            if ind%2 == 0:
+                res.append(
+                    ('right', level[ind+1])
+                )
+            else:
+                res.append(
+                    ('left', level[ind-1])
+                )
+            hash = self.levels[l+1][ceil(ind/2)-1]
+        return res
+            
+
+class Block:
+    def __init__(self, prevHash=None):
+        self.transactions = []
+        self.timestamp = int(time())
+        self.nonce = 0
+        self.miner = None
+        self.prevHash = prevHash
+
+    def addTransaction(self, transaction):
+        if type(transaction) == Transaction:
+            transaction = transaction.hash()
+        
+        self.transactions.append(transaction)
     
     def hash(self):
-        if len(self.l) % 2 != 0:
-            self.l.append(self.l[-1])
+        return sha256(str(
+            self.__dict__
+        ).encode()).hexdigest()
+    
+class BlockChain:
+    def __init__(self, db_name):
+        self.db_con = sqlite3.connect(db_name)
+        self.db_cur = self.db_con.cursor()
+        with open("setup.sql", 'r') as f:
+            self.db_cur.execute(f.read())
+    
+    def length(self):
+        return len(self.blocks)
+
+    def prevHash(self):
+        return self.blocks[0].prevHash()
         
--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,6 +1,5 @@
 import unittest
 from blockchain import *
-
 class TestAddressesTransactions(unittest.TestCase):
     @classmethod
     def setUpClass(self):
@@ -30,3 +29,30 @@ class TestAddressesTransactions(unittest.TestCase):
         self.assertTrue(self.t.verify())
         self.t.addrTo = 'tampered'
         self.assertFalse(self.t.verify())
+    
+    def test_block(self):
+        b = Block()
+        b.addTransaction(self.t)
+        b.addTransaction(self.t.hash())
+    
+    def test_merkle(self):
+        m = Merkle()
+        m.add('b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79')
+        self.assertEqual(
+            m.root(), 
+            'b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79'
+        )
+        m.add('3891f13300b85e89d403504b4c26abe3adf5f39420a2d111059423cb25b33b86')
+        self.assertEqual(
+            m.root(), 
+            '26086b9533dc45f23dfd7f4fe6cadf4a82ec83b310a49b828141a063eea0ea8e'
+        )
+        m.add('445c06f8335048bf3af883b047f79163c70083de3874e79ba1f7e621e0073579')
+        m.add('90984cc7ba5a96b3dcc55921ac4c7d7b344fcc37947a003cae10c475f4439377')
+        m.add('f7c3cc7a2377dabee2d71a54e5b1ca93dae1006887d0747c0978e051a948fc15')
+        self.assertEqual(
+            m.root(), 
+            '5c9c2ba5982276088d42e30ff2c7f9f5093336a5684dc8ce18788cc8e7a7805c'
+        )
+        
+        print(m.proof('445c06f8335048bf3af883b047f79163c70083de3874e79ba1f7e621e0073579'))
